3d5858525f7923cc63f940507dbf8dd3db379b57
Max Gordon <gordonmaxc@gmail.com>
2015-05-14 14:27:51 -0700

    can accept invite, destroys once used

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,31 @@
 class CharactersController < ApplicationController
+  def new
+    group = Group.find_by(id: params[:group_id])
+    @character = group.characters.new(group_id: group.id)
+  end
+
+  def create
+    invite_id = params[:invite_id].keys.first
+    invite = GroupInvite.find_by(id: invite_id)
+
+    @character = current_user.characters.new(character_params)
+    @character.group_id = params[:group_id]
+    if @character.save
+      invite.destroy
+      redirect_to group_path(params[:group_id]), notice: "Character Created Succesfully"
+    else
+      render :new
+    end
+  end
+
+  private
+  def character_params
+    params.require(:character).permit(
+      :name,
+      :race,
+      :money,
+      :exp,
+      :level
+    )
+  end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,7 @@
 class Character < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
+
+  validates :name, :race, :money, :exp, :level, presence: true
+  validates :name, uniqueness: {scope: :group_id}
 end
